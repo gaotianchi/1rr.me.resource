a7fbb18a65bf6d63f4e0f6bd6920dd51a5e6a9ed
gaotianchi <6159984@gmail.com>
2025-02-13 11:51:56 +0800

    添加用户表定义

---
+++ src/main/resources/sql/schemas/create_user_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE user (
+    username VARCHAR(255) UNIQUE  NOT NULL,
+    password VARCHAR(255) NOT NULL,
+
+    id INT NOT NULL AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL,
+
+    created_by VARCHAR(255) NOT NULL,
+    updated_by VARCHAR(255) NOT NULL,
+    deleted_by VARCHAR(255) NULL,
+
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX idx_user_username ON user (username);
