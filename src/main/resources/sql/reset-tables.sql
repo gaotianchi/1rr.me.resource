ed845b96caa5b5228ad46097a942a5a68cb41ec0
gaotianchi <6159984@gmail.com>
2025-02-14 10:32:15 +0800

    user 添加 email 相关字段

--- src/main/resources/sql/reset-tables.sql
+++ src/main/resources/sql/reset-tables.sql
@@ -5,6 +5,8 @@ CREATE TABLE user
 (
     username   VARCHAR(255) UNIQUE NOT NULL,
     password   VARCHAR(255)        NOT NULL,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    email_is_verified BOOLEAN DEFAULT FALSE,
 
     id         INT                 NOT NULL AUTO_INCREMENT,
 
