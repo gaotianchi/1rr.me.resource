00756159433fea6b0260a6b5025116f0bc478bd7
gaotianchi <6159984@gmail.com>
2025-02-14 17:41:11 +0800

    更新 application-dev-office.yaml 配置

--- src/main/java/com/gaotianchi/wtf/config/SecurityConfig.java
+++ src/main/java/com/gaotianchi/wtf/config/SecurityConfig.java
@@ -1,29 +1,31 @@
 package com.gaotianchi.wtf.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * @author gaotianchi
  * @since 2025/2/14 11:48
  **/
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // 配置请求的授权规则
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(AbstractAuthenticationFilterConfigurer::permitAll);
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        ;
         return http.build();
-
     }
 }
