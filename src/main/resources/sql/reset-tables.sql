d3e516cae9a4a796d16b0e6dad79d52b28506962
gaotianchi <6159984@gmail.com>
2025-02-15 18:03:14 +0800

    user 表添加字段用于标记用户是否使用了第三方库登录

--- src/main/resources/sql/reset-tables.sql
+++ src/main/resources/sql/reset-tables.sql
@@ -3,46 +3,47 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user
 (
-    username   VARCHAR(255) UNIQUE NOT NULL,
-    password   VARCHAR(255)        NOT NULL,
-    email      VARCHAR(255) UNIQUE NOT NULL,
-    email_is_verified BOOLEAN DEFAULT FALSE,
+    username              VARCHAR(255) UNIQUE NOT NULL,
+    password              VARCHAR(255)        NULL,
+    email                 VARCHAR(255) UNIQUE NOT NULL,
+    email_is_verified     BOOLEAN   DEFAULT FALSE,
+    use_third_party_login BOOLEAN   DEFAULT FALSE,
 
-    id         INT                 NOT NULL AUTO_INCREMENT,
+    id                    INT                 NOT NULL AUTO_INCREMENT,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP           NULL,
+    created_at            TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at            TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at            TIMESTAMP           NULL,
 
-    created_by VARCHAR(255)        NOT NULL,
-    updated_by VARCHAR(255)        NOT NULL,
-    deleted_by VARCHAR(255)        NULL,
+    created_by            VARCHAR(255)        NOT NULL,
+    updated_by            VARCHAR(255)        NOT NULL,
+    deleted_by            VARCHAR(255)        NULL,
 
-    is_deleted BOOLEAN   DEFAULT FALSE,
+    is_deleted            BOOLEAN   DEFAULT FALSE,
 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE link
 (
-    short_url  VARCHAR(255) UNIQUE NOT NULL,
-    original_url VARCHAR(255) NOT NULL,
-    user_id    INT                 NOT NULL,
-    expired_at TIMESTAMP NULL,
+    short_url    VARCHAR(255) UNIQUE NOT NULL,
+    original_url VARCHAR(255)        NOT NULL,
+    user_id      INT                 NOT NULL,
+    expired_at   TIMESTAMP           NULL,
 
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
 
-    id         INT                 NOT NULL AUTO_INCREMENT,
+    id           INT                 NOT NULL AUTO_INCREMENT,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP           NULL,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at   TIMESTAMP           NULL,
 
-    created_by VARCHAR(255)        NOT NULL,
-    updated_by VARCHAR(255)        NOT NULL,
-    deleted_by VARCHAR(255)        NULL,
+    created_by   VARCHAR(255)        NOT NULL,
+    updated_by   VARCHAR(255)        NOT NULL,
+    deleted_by   VARCHAR(255)        NULL,
 
-    is_deleted BOOLEAN   DEFAULT FALSE,
+    is_deleted   BOOLEAN   DEFAULT FALSE,
 
     PRIMARY KEY (id)
 );
