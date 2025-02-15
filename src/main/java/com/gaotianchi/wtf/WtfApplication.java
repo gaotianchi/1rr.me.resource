f4a772ed62ae24fcd8a72e55e407e906027eecc2
gaotianchi <6159984@gmail.com>
2025-02-15 22:30:18 +0800

    初步配置缓存

--- src/main/java/com/gaotianchi/wtf/WtfApplication.java
+++ src/main/java/com/gaotianchi/wtf/WtfApplication.java
@@ -2,8 +2,10 @@ package com.gaotianchi.wtf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WtfApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/gaotianchi/wtf/config/CacheConfig.java
@@ -0,0 +1,11 @@
+package com.gaotianchi.wtf.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author gaotianchi
+ * @since 2025/2/15 下午10:27
+ **/
+@Configuration
+public class CacheConfig {
+}
