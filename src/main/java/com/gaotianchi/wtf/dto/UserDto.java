d52c5078b670f61dfadfcac50b390255f7c87aa3
gaotianchi <6159984@gmail.com>
2025-02-15 20:55:17 +0800

    修复路径映射失败BUG

--- src/main/java/com/gaotianchi/wtf/dto/UserDto.java
+++ src/main/java/com/gaotianchi/wtf/dto/UserDto.java
@@ -1,17 +1,11 @@
 package com.gaotianchi.wtf.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * @author gaotianchi
  * @since 2025/2/15 下午5:40
  **/
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserDto {
 }
