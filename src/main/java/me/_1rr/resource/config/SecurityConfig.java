f26206bd3e6ab3067838a1a2b17faf6b28f5c9cc
gaotianchi <6159984@gmail.com>
2025-02-21 16:54:22 +0800

    实现创建 user 的方法

--- src/main/java/me/_1rr/resource/config/SecurityConfig.java
+++ src/main/java/me/_1rr/resource/config/SecurityConfig.java
@@ -6,8 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 /**
  * @author gaotianchi
@@ -21,8 +26,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         JwtIssuerAuthenticationManagerResolver authenticationManagerResolver = JwtIssuerAuthenticationManagerResolver.fromTrustedIssuers("https://accounts.google.com", "https://github.com");
         http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/test/**")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/users")
                         .permitAll()
                         .requestMatchers(HttpMethod.GET, "/{code:[a-zA-Z0-9]+}")
                         .permitAll()
@@ -30,5 +39,17 @@ public class SecurityConfig {
                         .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.authenticationManagerResolver(authenticationManagerResolver));
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
---
+++ src/main/java/me/_1rr/resource/dao/UserDao.java
@@ -0,0 +1,12 @@
+package me._1rr.resource.dao;
+
+import me._1rr.resource.dao.base.UserBaseDao;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author gaotianchi
+ * @since 2025/2/21 下午4:30
+ **/
+@Mapper
+public interface UserDao extends UserBaseDao {
+}
