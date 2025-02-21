b3e928c36abab5a86d3b816be7baaa2df8ac8aba
gaotianchi <6159984@gmail.com>
2025-02-21 16:54:22 +0800

    实现创建 user 的方法

--- src/main/java/me/_1rr/resource/dto/UserDto.java
+++ src/main/java/me/_1rr/resource/dto/UserDto.java
@@ -8,4 +8,8 @@ import lombok.Data;
  **/
 @Data
 public class UserDto {
+    private String username;
+    private String password;
+    private String email;
+    private Integer useThirdPartyLogin;
 }
--- src/main/java/me/_1rr/resource/service/impl/UserServiceImpl.java
+++ src/main/java/me/_1rr/resource/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package me._1rr.resource.service.impl;
 
+import me._1rr.resource.dao.UserDao;
 import me._1rr.resource.dto.UserDto;
+import me._1rr.resource.entity.User;
 import me._1rr.resource.service.UserService;
 import me._1rr.resource.vo.UserVo;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,9 +14,29 @@ import org.springframework.stereotype.Service;
  **/
 @Service("userService")
 public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @Override
     public String createNewUser(UserDto userDto) {
-        return "";
+        User user = User
+                .builder()
+                .username(userDto.getUsername())
+                .password(userDto.getPassword())
+                .email(userDto.getEmail())
+                .useThirdPartyLogin(userDto.getUseThirdPartyLogin())
+                .build()
+                ;
+        try {
+            userDao.insertUser(user);
+        } catch (DuplicateKeyException e) {
+            return userDto.getUsername();
+        }
+        return userDto.getUsername();
     }
 
     @Override
