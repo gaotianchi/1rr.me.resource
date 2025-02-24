306c2f7984001222291211e4a09762b3a1d6a578
gaotianchi <6159984@gmail.com>
2025-02-24 21:01:48 +0800

    更新安全配置以允许对 /api/links 的访问，扩展 LinkVo 类以包含更多字段，优化哈希生成逻辑以确保生成的短链接长度为6

--- src/main/java/me/_1rr/resource/service/impl/CoreServiceImpl.java
+++ src/main/java/me/_1rr/resource/service/impl/CoreServiceImpl.java
@@ -1,12 +1,12 @@
 package me._1rr.resource.service.impl;
 
+import com.google.common.hash.Hashing;
 import me._1rr.resource.dto.LinkDto;
 import me._1rr.resource.exception.InvalidPasswordException;
 import me._1rr.resource.exception.LinkExpiredException;
 import me._1rr.resource.exception.LinkNotFoundException;
 import me._1rr.resource.service.CacheService;
 import me._1rr.resource.service.CoreService;
-import com.google.common.hash.Hashing;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
@@ -55,20 +55,33 @@ public class CoreServiceImpl implements CoreService {
                 .hashString(originalUrl, StandardCharsets.UTF_8)
                 .asInt()
                 ;
-        return toBase62(hash).substring(0, 6);
+
+        hash = Math.abs(hash);
+        StringBuilder base62 = new StringBuilder(toBase62(hash));
+        while (base62.length() < 6) {
+            base62.insert(0, "0");
+        }
+        return base62.substring(0, 6);
     }
 
     private String toBase62(int num) {
         String CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            sb.append(CHARSET.charAt(num % 62));
-            num /= 62;
+
+        if (num == 0) {
+            sb.append(CHARSET.charAt(0));
+        } else {
+            while (num > 0) {
+                sb.append(CHARSET.charAt(num % 62));
+                num /= 62;
+            }
         }
+
         return sb
                 .reverse()
                 .toString();
     }
+
 
     private boolean isLinkExpired(LinkDto linkDto) {
         LocalDateTime expireAt = linkDto.getExpireAt();
